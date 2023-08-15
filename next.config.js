da86ddd506a916bf4176b51b4972859a5099bbe5
Shayaan Haq <haqskids@gmail.com>
2023-08-15 11:53:43 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ module.exports = {
     return [
       {
         // matching all API routes
-        source: "/ai_model/:path*",
+        source: "/ai_model_2/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
